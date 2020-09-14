cea3d59795236175357bfa9eb0790475b23ca36f
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-09-14 14:30:17 -0300

    Itens de lista para o contêiner "Content"

--- containers/content.js
+++ containers/content.js
@@ -6,8 +6,15 @@ export default function Content() {
       <div className={styles.wrapper}>
         <div className={styles.card}>
           <div>
-            <h2>Sintomas da doença</h2>
+            <h2>Sintomas da depressão</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum dui id fermentum gravida. Mauris ut porttitor justo, at fringilla urna. Morbi congue feugiat orci, ut rutrum turpis rhoncus eu. Phasellus ex quam, iaculis nec massa in, pretium interdum ante. Praesent mauris lectus, laoreet nec mollis vitae, semper a ipsum. Mauris at enim tortor. Sed quis purus id augue facilisis commodo blandit eget orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc blandit quis mauris quis elementum. In tincidunt euismod cursus.</p>
+            <ul>
+              <li>Sintoma 1</li>
+              <li>Sintoma 2</li>
+              <li>Sintoma 3</li>
+              <li>Sintoma 4</li>
+              <li>Sintoma 5</li>
+            </ul>
           </div>
           <picture>
             <source srcSet='images/paciente-em-acompanhamento.webp' type='image/webp'/>
@@ -20,7 +27,7 @@ export default function Content() {
             <img src='images/paciente-recuperada.jpg' alt='Acompanhamento de paciente' loading='lazy'/>
           </picture>
           <div>
-            <h2>Tratamento da doença</h2>
+            <h2>Tratamento da depressão</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum dui id fermentum gravida. Mauris ut porttitor justo, at fringilla urna. Morbi congue feugiat orci, ut rutrum turpis rhoncus eu. Phasellus ex quam, iaculis nec massa in, pretium interdum ante. Praesent mauris lectus, laoreet nec mollis vitae, semper a ipsum. Mauris at enim tortor. Sed quis purus id augue facilisis commodo blandit eget orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc blandit quis mauris quis elementum. In tincidunt euismod cursus.</p>
           </div>
         </div>
