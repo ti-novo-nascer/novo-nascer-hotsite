5a9628760d36ca7f006d84ee10775b6114cb0c63
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-09-15 14:34:55 -0300

    Criação do contêiner "Testimonials"

---
+++ containers/testimonials.js
@@ -0,0 +1,41 @@
+import Carousel from 'react-bootstrap/Carousel'
+import Prev from '../components/prev'
+import Next from '../components/next'
+import styles from '../styles/testimonials.module.scss'
+
+const testimonials = [
+  { author: 'Autor um', text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum dui id fermentum gravida. Mauris ut porttitor justo, at fringilla urna. Morbi congue feugiat orci, ut rutrum turpis rhoncus eu. Phasellus ex quam, iaculis nec massa in, pretium interdum ante. Praesent mauris lectus, laoreet nec mollis vitae, semper a ipsum. Mauris at enim tortor. Sed quis purus id augue facilisis commodo blandit eget orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc blandit quis mauris quis elementum. In tincidunt euismod cursus."' },
+  { author: 'Autor dois', text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum dui id fermentum gravida. Mauris ut porttitor justo, at fringilla urna. Morbi congue feugiat orci, ut rutrum turpis rhoncus eu. Phasellus ex quam, iaculis nec massa in, pretium interdum ante. Praesent mauris lectus, laoreet nec mollis vitae, semper a ipsum. Mauris at enim tortor. Sed quis purus id augue facilisis commodo blandit eget orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc blandit quis mauris quis elementum. In tincidunt euismod cursus."' },
+  { author: 'Autor três', text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum dui id fermentum gravida. Mauris ut porttitor justo, at fringilla urna. Morbi congue feugiat orci, ut rutrum turpis rhoncus eu. Phasellus ex quam, iaculis nec massa in, pretium interdum ante. Praesent mauris lectus, laoreet nec mollis vitae, semper a ipsum. Mauris at enim tortor. Sed quis purus id augue facilisis commodo blandit eget orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc blandit quis mauris quis elementum. In tincidunt euismod cursus."' },
+  { author: 'Autor quatro', text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum dui id fermentum gravida. Mauris ut porttitor justo, at fringilla urna. Morbi congue feugiat orci, ut rutrum turpis rhoncus eu. Phasellus ex quam, iaculis nec massa in, pretium interdum ante. Praesent mauris lectus, laoreet nec mollis vitae, semper a ipsum. Mauris at enim tortor. Sed quis purus id augue facilisis commodo blandit eget orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc blandit quis mauris quis elementum. In tincidunt euismod cursus."' },
+  { author: 'Autor cinco', text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum dui id fermentum gravida. Mauris ut porttitor justo, at fringilla urna. Morbi congue feugiat orci, ut rutrum turpis rhoncus eu. Phasellus ex quam, iaculis nec massa in, pretium interdum ante. Praesent mauris lectus, laoreet nec mollis vitae, semper a ipsum. Mauris at enim tortor. Sed quis purus id augue facilisis commodo blandit eget orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc blandit quis mauris quis elementum. In tincidunt euismod cursus."' },
+  { author: 'Autor seis', text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum dui id fermentum gravida. Mauris ut porttitor justo, at fringilla urna. Morbi congue feugiat orci, ut rutrum turpis rhoncus eu. Phasellus ex quam, iaculis nec massa in, pretium interdum ante. Praesent mauris lectus, laoreet nec mollis vitae, semper a ipsum. Mauris at enim tortor. Sed quis purus id augue facilisis commodo blandit eget orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc blandit quis mauris quis elementum. In tincidunt euismod cursus."' }
+]
+
+export default function Testimonials() {
+  return (
+    <section className={styles.testimonials}>
+      <div className={styles.wrapper}>
+        <div className={styles.sectionHead}>
+          <h2>Depoimentos</h2>
+          <h3>A vivência de nossos clientes e familiares</h3>
+        </div>
+        <Carousel
+          className={styles.sectionBody}
+          prevIcon={<Prev/>}
+          nextIcon={<Next/>}
+          indicators={false}
+        >
+          {testimonials.map(({ author, text }, idx) => {
+            return (
+              <Carousel.Item key={idx} className={styles.carouselItem}>
+                <p>{text}</p>
+                <span>{author}</span>
+              </Carousel.Item>
+            )
+          })}
+        </Carousel>
+      </div>
+    </section>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,13 @@ import Presentation from '../containers/presentation'
 import Quote from '../containers/quote'
 import Content from '../containers/content'
 import Service from '../containers/service'
+import Testimonials from '../containers/testimonials'
 import ContactForm from '../containers/contact-form'
 import Footer from '../containers/footer'
 
 const pageData = {
-  title: 'Como Tratar a Doença | Novo Nascer',
-  description: 'A Novo Nascer oferece a você um tratamento integral e humanizado para a doença',
+  title: 'Tratamento para a Depressão da Novo Nascer',
+  description: 'A Novo Nascer oferece a você um tratamento integral e humanizado para a depressão',
   url: 'http://localhost:3000/',
   image: 'http://localhost:3000/images/paciente-em-monitoramento.jpg'
 }
@@ -23,6 +24,7 @@ export default function Home() {
       <Quote/>
       <Content/>
       <Service/>
+      <Testimonials/>
       <ContactForm/>
       <Footer/>
     </>
