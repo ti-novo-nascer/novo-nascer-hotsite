4fefdc73e53c71ff6fbc5283ddaeb2e32a5bda92
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-09-24 17:11:46 -0300

    Criação do contêiner "FixedHeader"

---
+++ containers/fixed-header.js
@@ -0,0 +1,42 @@
+import classNames from 'classnames'
+import Fade from 'react-reveal/Fade'
+import ExternalLink from '../components/external-link'
+import TwentyFourHours from '../components/24-hours'
+import SendEmail from '../components/send-email'
+import styles from '../styles/fixed-header.module.scss'
+
+export default function FixedHeader({ className, show }) {
+  return (
+    <Fade top duration={400}>
+      <header
+        className={
+          classNames({
+            [className]: true,
+            [styles.fixedHeader]: true,
+            [styles.hide]: !show
+          })
+        }
+      >
+        <div className={styles.wrapper}>
+          <div className={styles.menu}>
+            <a href='#contact-form'>
+              <button>
+                <SendEmail/>
+                <span>contato@novonascer.com.br</span>
+              </button>
+            </a>
+            <ExternalLink href='https://www.novonascer.com.br/'>
+              <img src='images/novo-nascer-simbolo-azul.svg' alt='Logo da Clínica Hospitalar Novo Nascer'/>
+            </ExternalLink>
+            <ExternalLink href='https://wa.me/5581973163700'>
+              <button>
+                <TwentyFourHours/>
+                <span>(81) 97316-3700</span>
+              </button>
+            </ExternalLink>
+          </div>
+        </div>
+      </header>
+    </Fade>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react'
 import Seo from '../containers/seo'
 import Header from '../containers/header'
+import FixedHeader from '../containers/fixed-header'
 import Presentation from '../containers/presentation'
 import CallToAction from '../containers/call-to-action'
 import Content from '../containers/content'
@@ -9,6 +11,7 @@ import Service from '../containers/service'
 import Testimonials from '../containers/testimonials'
 import ContactForm from '../containers/contact-form'
 import Footer from '../containers/footer'
+import styles from '../styles/home.module.scss'
 
 const pageData = {
   title: 'Tratamento para a Depressão da Novo Nascer',
@@ -18,10 +21,23 @@ const pageData = {
 }
 
 export default function Home() {
+  const [showFixedHeader, setShowFixedHeader] = useState(false)
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 600 && showFixedHeader === false) {
+          setShowFixedHeader(true)
+        } else if (window.pageYOffset <= 600 && showFixedHeader === true) {
+          setShowFixedHeader(false)
+        }
+      })
+    }
+  }, [showFixedHeader, setShowFixedHeader])
   return (
     <>
       <Seo {...pageData}/>
       <Header/>
+      <FixedHeader className={styles.fixedHeader} show={showFixedHeader}/>
       <Presentation/>
       <CallToAction/>
       <Content/>
