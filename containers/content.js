13e06cbb3c58b5367c86570a53bf9adef994a902
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-16 14:27:03 -0300

    Adição de conteúdo no contêiner "Content"

--- containers/content.js
+++ containers/content.js
@@ -12,17 +12,29 @@ export default function Content() {
           </div>
           <div className={styles.sectionBody}>
             <img src='images/paciente-em-acompanhamento.webp' alt='Acompanhamento de paciente' loading='lazy'/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum dui id fermentum gravida. Mauris ut porttitor justo, at fringilla urna. Morbi congue feugiat orci, ut rutrum turpis rhoncus eu. Phasellus ex quam, iaculis nec massa in, pretium interdum ante. Praesent mauris lectus, laoreet nec mollis vitae, semper a ipsum. Mauris at enim tortor. Sed quis purus id augue facilisis commodo blandit eget orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc blandit quis mauris quis elementum. In tincidunt euismod cursus.</p>
-            <ul>
-              <li>Humor triste, ansioso ou “vazio” persistente</li>
-              <li>Sentimentos de desesperança, luto ou pessimismo</li>
-              <li>Sentimentos de culpa, inutilidade ou desamparo</li>
-              <li>Perda de interesse ou prazer pela vida, hobbies e atividades</li>
-              <li>Diminuição da energia ou fadiga</li>
-              <li>Sentir-se inquieto ou ter problemas para ficar sentado</li>
-              <li>Dificuldade de concentração, lembrança ou tomada de decisões</li>
-              <li>Dificuldade para dormir, despertar de manhã cedo ou dormir demais</li>
-            </ul>
+            <div className={styles.text}>
+              <p>O termo depressão vem sendo utilizado para uma grande variedade de questões, como tristeza, reações a certas circunstâncias da vida e até a outros distúrbios emocionais. A depressão de fato, porém, é um transtorno afetivo que envolve alterações nos neurotransmissores cerebrais e se apresenta com rebaixamento do humor, redução de energia e prejuízo de atividades em geral.</p>
+              <p>Depressão e tristeza têm características distintas. A tristeza é um sentimento universal, natural e fisiológico que expressa dor ou sofrimento e, ao contrário da depressão, não compromete significativamente outras funções mentais nem o funcionamento global do indivíduo.</p>
+              <p>Como qualquer transtorno emocional, a depressão pode afetar qualquer pessoa, independentemente de sua conta bancária, situação de poder ou fama. Quantas pessoas aparentam viver em um estado de felicidade na superfície, quando na verdade sentem constantemente sofrimento profundo, vazio, solidão?</p>
+              <p>A depressão atinge duas vezes mais mulheres do que homens e pode ser classificada em graus: leve, moderado e grave. Deve ser tratado adequadamente e o quanto mais cedo possível e o entendimento alheio de ser "uma besteira", "falta do que fazer" ou que palavras como "vamos animar que passa" ou "pense positivo" apenas afastam mais ainda a pessoa, que precisa de ajuda efetiva.</p>
+              <p>A internação em casos de depressão ocorre quando o paciente já está em tratamento no consultório e o resultado não aparece. Tentativa de suicídio, incapacidade de levantar da cama, de trabalhar e até mesmo de tomar qualquer tipo de medicação são outros indicativos de que a internação deve ser feita o quanto antes.</p>
+              <p>Com a internação, o paciente alcança a estabilização do quadro. Essa conquista resguarda-o de tentativas de suicídio ou de improdutividade, quadro que esse comum em pacientes que não procuram tratamento ou com tratamento inadequado para gravidade de sua situação.</p>
+              <p>Alguns sintomas da depressão são:</p>
+              <ul>
+                <li>Desânimo e choro</li>
+                <li>Sensação persistente de tristeza, angústia e/ou vazio</li>
+                <li>Perda de capacidade de sentir prazer</li>
+                <li>Baixa autoestima</li>
+                <li>Plano ou tentativa de suicídio</li>
+                <li>Pensamentos sobre morte ou suicídio</li>
+                <li>Diminuição da libido</li>
+                <li>Insegurança, medos e indecisões</li>
+                <li>Inquietação, ansiedade ou irritabilidade</li>
+                <li>Falta de sentido na vida</li>
+                <li>Dores crônicas ou sintomas físicos difusos e persistentes</li>
+                <li>Insônia ou sonolência excessiva</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
