45399f18ab48efc94d85d22b58c8b7ce6276469d
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-17 16:33:07 -0300

    Adição de TypeScript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from 'next/app'
 import '../styles/globals.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps}/>
 }
 
