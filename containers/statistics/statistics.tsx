c919360474af543a83a533abc55f5b330150fe8c
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-18 10:06:33 -0300

    Conversão do "Statistics" para TypeScript

--- containers/statistics/statistics.tsx
+++ containers/statistics/statistics.tsx
@@ -1,17 +1,17 @@
 import Fade from 'react-reveal/Fade'
-import styles from '../styles/statistics.module.scss'
+import styles from './statistics.module.scss'
 
 export default function Statistics() {
   return (
     <Fade bottom duration={2000}>
       <div className={styles.statistics}>
         <div className={styles.wrapper}>
-          <div className={styles.sectionHead}>
+          <div className={styles.head}>
             <h2>Depressão em números</h2>
             <h3>Os números mostram a proporção da depressão no mundo</h3>
           </div>
-          <div className={styles.sectionBody}>
-            <img src='images/depressao-estatistica.webp' alt='Dados sobre a depressão' loading='lazy'/>
+          <div className={styles.body}>
+            <img src='/depressao-estatistica.webp' alt='Dados sobre a depressão' loading='lazy'/>
           </div>
         </div>
       </div>
