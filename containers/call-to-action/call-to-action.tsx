7d72e902ca29cd0a5b9a0ca69fb068f8daa1d64c
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-18 10:37:19 -0300

    Conversão do "Call To Action" para TypeScript

--- containers/call-to-action/call-to-action.tsx
+++ containers/call-to-action/call-to-action.tsx
@@ -1,16 +1,16 @@
 import Fade from 'react-reveal/Fade'
-import ExternalLink from '../components/external-link'
-import styles from '../styles/call-to-action.module.scss'
+import ExternalLink from '../../components/external-link'
+import styles from './call-to-action.module.scss'
 
 export default function CallToAction() {
   return (
     <div className={styles.callToAction}>
       <Fade duration={4000}>
         <div className={styles.wrapper}>
-          <div className={styles.sectionHead}>
+          <div className={styles.head}>
             <h2>Está precisando de ajuda?</h2>
           </div>
-          <div className={styles.sectionBody}>
+          <div className={styles.body}>
             <ExternalLink href='https://wa.me/5581973163700'>
               <button>
                 <span>Converse conosco agora!</span>
