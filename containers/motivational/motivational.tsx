2a9de50a74d0918bc246958356117cf41fcc053b
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-18 10:43:06 -0300

    Conversão do "Motivational" para TypeScript

--- containers/motivational/motivational.tsx
+++ containers/motivational/motivational.tsx
@@ -1,16 +1,16 @@
 import Fade from 'react-reveal/Fade'
-import ExternalLink from '../components/external-link'
-import styles from '../styles/motivational.module.scss'
+import ExternalLink from '../../components/external-link'
+import styles from './motivational.module.scss'
 
 export default function Motivational() {
   return (
     <div className={styles.motivational}>
       <Fade duration={4000}>
         <div className={styles.wrapper}>
-          <div className={styles.sectionHead}>
+          <div className={styles.head}>
             <h2>Você não está sozinho!</h2>
           </div>
-          <div className={styles.sectionBody}>
+          <div className={styles.body}>
             <ExternalLink href='https://wa.me/5581973163700'>
               <button>
                 <span>Peça ajuda agora!</span>
