e78d516dc98d155e8e85c54e6ee02e51090e2008
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-11-04 15:02:12 -0300

    Mudar email de formulário

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -17,7 +17,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
   const message = {
-    to: 'contatosite@novonascer.com.br',
+    to: 'clinicanovonascer@gmail.com',
     from: 'ti@novonascer.com.br',
     subject: 'CONTATO DE CLIENTE',
     html: emailTemplate(host, name, email, phone, city, healthPlan, details)
