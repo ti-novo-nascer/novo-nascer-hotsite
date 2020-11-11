f97e6c64e289e1bf50ad19b2e623681cbd3348c6
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-11-11 13:56:25 -0300

    Add more meta tags

--- containers/seo/seo.tsx
+++ containers/seo/seo.tsx
@@ -20,6 +20,8 @@ export default function Seo({
 }: SeoProps) {
   return (
     <Head>
+      <meta charSet='utf-8'/>
+      <meta name='viewport' content='width=device-width, initial-scale=1'/>
       <meta name='author' content='Lucas Alves Costa'/>
       {/* SERP tags */}
       <title>{title}</title>
